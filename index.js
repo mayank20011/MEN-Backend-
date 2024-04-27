cf299b8471ea0b85e4f01897317a0f76ab4b3637
mayank20011 <mayank.behl.mb1@gmail.com>
2024-04-27 14:36:39 +0530

    Added bootstrap and nodemon, express dependencies

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import {sum,diff,product} from './lib.js';
+import express from 'express';
 
 let a=[1,2,3,4,5,6,7,8,9,10];
 
 console.log(`sum of Each Element from Array is: ${sum(...a)} and product is : ${product(...a)} and difference is: ${diff(...a)}`);
 
-
+console.log('Hello, Whaat is your name?');
+const server=express();
+server.listen(3000);
