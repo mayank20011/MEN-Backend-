826964bbf3df105fe3fb1f72e74dd541061dde7d
mayank20011 <mayank.behl.mb1@gmail.com>
2024-04-27 13:20:58 +0530

    Using Npm

---
+++ index2.js
@@ -0,0 +1,16 @@
+import * as fs from "node:fs";
+
+console.log("program start");
+// for synchronous file reading
+// console.log(fs.readFileSync('file.txt','utf-8'));
+
+// for asynchronous file reading
+
+fs.readFile("file.txt", "utf-8", (err, txt) => {
+  if (err) {
+    console.log("Something Went Wrong !!!");
+  } else {
+    console.log(txt);
+  }
+});
+console.log("program end");
