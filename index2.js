f7d09ae79a739c643f3db12fe54efc23510d9ec9
mayank20011 <mayank.behl.mb1@gmail.com>
2024-05-02 12:52:07 +0530

    modified index2.js

--- index2.js
+++ index2.js
@@ -3,10 +3,12 @@ import * as fs from 'node:fs';
 
 const indexfile=fs.readFileSync('index.html','utf-8');
 const jsondata=fs.readFileSync('Dummydata.json','utf-8');
+
+const jsondatastring=jsondata.toString();
+// console.log(indexstring);
 const server=http.createServer((req,res)=>
 {
-  // res.end("hello");
-  res.end(indexfile.toString());
-});
+  res.end(`<h1>${jsondatastring}</h1>`);
 
+});
 server.listen(8080);
