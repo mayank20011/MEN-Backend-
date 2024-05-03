9122034e6b0dcc2b5e7b3b8c32c23c2970e4e5e6
mayank20011 <mayank.behl.mb1@gmail.com>
2024-05-03 15:38:57 +0530

    made somechanges and finalized this branch, no need to touch it anymore

--- index2.js
+++ index2.js
@@ -1,14 +1,12 @@
 import * as http from 'node:http';
 import * as fs from 'node:fs';
 
-const indexfile=fs.readFileSync('index.html','utf16le');
-const jsondata=fs.readFileSync('Dummydata.json','utf16le');
+const indexfile=fs.readFileSync('index.html','utf16le').trim();
 
-const jsondatastring=jsondata.toString();
-// console.log(indexstring);
 const server=http.createServer((req,res)=>
 {
-  res.end(`<h1>${jsondatastring}</h1>`);
+  res.setHeader('Content-Type','text/html');
+  res.end(indexfile.replace('**Helloworld**',"Hello Mayank"));
 
 });
 server.listen(8080);
