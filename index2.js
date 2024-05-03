1975837e2c419710a4857bc3020778e1b5264d96
mayank20011 <mayank.behl.mb1@gmail.com>
2024-05-03 13:31:46 +0530

    Useless Commit

--- index2.js
+++ index2.js
@@ -1,12 +1,14 @@
 import * as http from 'node:http';
 import * as fs from 'node:fs';
 
-const indexfile=fs.readFileSync('index.html','utf-8');
-const jsondata=fs.readFileSync('Dummydata.json','utf-8');
+const indexfile=fs.readFileSync('index.html','utf16le');
+const jsondata=fs.readFileSync('Dummydata.json','utf16le');
+
+const jsondatastring=jsondata.toString();
+// console.log(indexstring);
 const server=http.createServer((req,res)=>
 {
-  // res.end("hello");
-  res.end(indexfile.toString());
-});
+  res.end(`<h1>${jsondatastring}</h1>`);
 
+});
 server.listen(8080);
